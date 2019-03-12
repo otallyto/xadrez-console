0913976090654da6d7790f4d0ef63daa219c9e5c
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2019-03-11 21:18:10 -0400

    Tratando erros com excecao personalizada

---
+++ xadrez-console/Tabuleiro/TabuleiroExcption.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class TabuleiroExcption : Exception
+    {
+        public TabuleiroExcption (string msg) : base(msg)
+        {
+
+        }
+    }
+}
